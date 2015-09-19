dccc603f336d41045f633581e7c33696e177d570
Enrique <evaleriano97@gmail.com>
2015-09-19 15:39:05 -0500

    Update interfaz 2

--- src/Enemigo.java
+++ src/Enemigo.java
@@ -1,4 +1,8 @@
 
 public class Enemigo extends Personaje {
-
+	public int getPosX(){return posX;}
+	public int getPosY(){return posY;}
+	public int getAncho(){return ancho;}
+	public int getAltura(){return altura;}
+	public char getSprite(){return sprite;}
 }
