77f675550b8eff69c960175e2275e5e3cd0e5307
Slsvcn <sebastian.solari.16@gmail.com>
2015-10-06 15:36:12 -0500

    Juego sin warnings

--- src/vista/Juego.java
+++ src/vista/Juego.java
@@ -1,14 +1,5 @@
 package vista;
-
-import controlador.*;
-//import modelo.*;
-
-import java.io.IOException;
-import java.util.*;
-
 import javax.swing.SwingUtilities;
-
-import java.lang.String;
 
 public class Juego {
     public static void main(String[] args) {
